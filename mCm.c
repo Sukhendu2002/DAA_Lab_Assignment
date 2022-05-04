eef1be31f87b2c33a42589ca60cc80f9774adf80
Sukhendu Sekhar Guria <76804228+Sukhendu2002@users.noreply.github.com>
2022-05-04 18:43:50 +0530

    Update mCm.c

--- mCm.c
+++ mCm.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
-#include<limits.h>
- 
+#include <stdio.h>
+#include <limits.h>
+
 // Matrix Ai has dimension p[i-1] x p[i] for i = 1..n
- 
+
 int MatrixChainMultiplication(int p[], int i, int j)
 {
     if (i == j)
@@ -10,45 +10,46 @@ int MatrixChainMultiplication(int p[], int i, int j)
     int k;
     int min = INT_MAX;
     int count;
-  
+
     // place parenthesis at different places between first
     // and last matrix, recursively calculate count of
     // multiplications for each parenthesis placement and
     // return the minimum count
-    for (k = i; k < j; k++) {
-        count = MatrixChainOrder(p, i, k) + 
-                MatrixChainOrder(p, k + 1, j) + 
+    for (k = i; k < j; k++)
+    {
+        count = MatrixChainMultiplication(p, i, k) +
+                MatrixChainMultiplication(p, k + 1, j) +
                 p[i - 1] * p[k] * p[j];
-  
+
         if (count < min)
             min = count;
     }
-  
+
     // Return minimum count
     return min;
 }
- 
+
 int main()
 {
-    int n,i;
+    int n, i;
     printf("Enter number of matrices\n");
-    scanf("%d",&n);
- 
+    scanf("%d", &n);
+
     n++;
- 
+
     int arr[n];
- 
+
     printf("Enter dimensions \n");
- 
-    for(i=0;i<n;i++)
+
+    for (i = 0; i < n; i++)
     {
-        printf("Enter d%d :: ",i);
-        scanf("%d",&arr[i]);
+        printf("Enter d%d :: ", i);
+        scanf("%d", &arr[i]);
     }
- 
-    int size = sizeof(arr)/sizeof(arr[0]);
- 
-    printf("Minimum number of multiplications is %d ", MatrixChainMultiplication(arr, size));
- 
+
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Minimum number of multiplications is %d ", MatrixChainMultiplication(arr, 1, size - 1));
+
     return 0;
 }
