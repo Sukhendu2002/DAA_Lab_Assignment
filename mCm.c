6938a42f25d0a4e39d98464b4ca7c072f612e168
Sukhendu Sekhar Guria <76804228+Sukhendu2002@users.noreply.github.com>
2022-05-04 18:40:54 +0530

    Update mCm.c

--- mCm.c
+++ mCm.c
@@ -3,35 +3,29 @@
  
 // Matrix Ai has dimension p[i-1] x p[i] for i = 1..n
  
-int MatrixChainMultiplication(int p[], int n)
+int MatrixChainMultiplication(int p[], int i, int j)
 {
-    int m[n][n];
-    int i, j, k, L, q;
- 
-    for (i=1; i<n; i++)
-        m[i][i] = 0;    //number of multiplications are 0(zero) when there is only one matrix
- 
-    //Here L is chain length. It varies from length 2 to length n.
-    for (L=2; L<n; L++)
-    {
-        for (i=1; i<n-L+1; i++)
-        {
-            j = i+L-1;
-            m[i][j] = INT_MAX;  //assigning to maximum value
- 
-            for (k=i; k<=j-1; k++)
-            {
-                q = m[i][k] + m[k+1][j] + p[i-1]*p[k]*p[j];
-                if (q < m[i][j])
-                {
-                    m[i][j] = q;    //if number of multiplications found less that number will be updated.
-                }
-            }
-        }
+    if (i == j)
+        return 0;
+    int k;
+    int min = INT_MAX;
+    int count;
+  
+    // place parenthesis at different places between first
+    // and last matrix, recursively calculate count of
+    // multiplications for each parenthesis placement and
+    // return the minimum count
+    for (k = i; k < j; k++) {
+        count = MatrixChainOrder(p, i, k) + 
+                MatrixChainOrder(p, k + 1, j) + 
+                p[i - 1] * p[k] * p[j];
+  
+        if (count < min)
+            min = count;
     }
- 
-    return m[1][n-1];   //returning the final answer which is M[1][n]
- 
+  
+    // Return minimum count
+    return min;
 }
  
 int main()
