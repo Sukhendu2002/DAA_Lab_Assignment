9a856396bcd9538beda34aa3a4a8490186f6657c
Sukhendu Sekhar Guria <76804228+Sukhendu2002@users.noreply.github.com>
2022-05-04 18:22:28 +0530

    Update mCm.c

--- mCm.c
+++ mCm.c
@@ -1,32 +1,60 @@
-#include <limits.h>
-#include <stdio.h>
-
-int MatrixChainOrder(int p[], int i, int j)
-{
-    if (i == j)
-        return 0;
-    int k;
-    int min = INT_MAX;
-    int count;
-    for (k = i; k < j; k++)
-    {
-        count = MatrixChainOrder(p, i, k)
-                + MatrixChainOrder(p, k + 1, j)
-                + p[i - 1] * p[k] * p[j];
+#include<stdio.h>
+#include<limits.h>
  
-        if (count < min)
-            min = count;
+// Matrix Ai has dimension p[i-1] x p[i] for i = 1..n
+ 
+int MatrixChainMultiplication(int p[], int n)
+{
+    int m[n][n];
+    int i, j, k, L, q;
+ 
+    for (i=1; i<n; i++)
+        m[i][i] = 0;    //number of multiplications are 0(zero) when there is only one matrix
+ 
+    //Here L is chain length. It varies from length 2 to length n.
+    for (L=2; L<n; L++)
+    {
+        for (i=1; i<n-L+1; i++)
+        {
+            j = i+L-1;
+            m[i][j] = INT_MAX;  //assigning to maximum value
+ 
+            for (k=i; k<=j-1; k++)
+            {
+                q = m[i][k] + m[k+1][j] + p[i-1]*p[k]*p[j];
+                if (q < m[i][j])
+                {
+                    m[i][j] = q;    //if number of multiplications found less that number will be updated.
+                }
+            }
+        }
     }
-    return min;
+ 
+    return m[1][n-1];   //returning the final answer which is M[1][n]
+ 
 }
+ 
 int main()
 {
-    int arr[] = { 1, 2, 3, 4, 3 };
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n,i;
+    printf("Enter number of matrices\n");
+    scanf("%d",&n);
  
-    printf("Minimum number of multiplications is %d ",
-           MatrixChainOrder(arr, 1, n - 1));
+    n++;
  
-    getchar();
+    int arr[n];
+ 
+    printf("Enter dimensions \n");
+ 
+    for(i=0;i<n;i++)
+    {
+        printf("Enter d%d :: ",i);
+        scanf("%d",&arr[i]);
+    }
+ 
+    int size = sizeof(arr)/sizeof(arr[0]);
+ 
+    printf("Minimum number of multiplications is %d ", MatrixChainMultiplication(arr, size));
+ 
     return 0;
 }
